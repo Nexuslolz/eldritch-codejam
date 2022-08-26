6d8de3015b290998bccecb7fbf963d6736e1df71
Nexuslolz <a.klyosov@inbox.ru>
2022-08-26 17:48:11 +0500

    fix: fixed assets of the project

--- assets/MythicCards/brown/index.js
+++ assets/MythicCards/brown/index.js
@@ -1,47 +1,47 @@
-import brown1 from './brown1.png';
-import brown2 from './brown2.png';
-import brown3 from './brown3.png';
-import brown4 from './brown4.png';
-import brown5 from './brown5.png';
-import brown6 from './brown6.png';
-import brown7 from './brown7.png';
-import brown8 from './brown8.png';
-import brown9 from './brown9.png';
-import brown10 from './brown10.png';
-import brown11 from './brown11.png';
-import brown12 from './brown12.png';
-import brown13 from './brown13.png';
-import brown14 from './brown14.png';
-import brown15 from './brown15.png';
-import brown16 from './brown16.png';
-import brown17 from './brown17.png';
-import brown18 from './brown18.png';
-import brown19 from './brown19.png';
-import brown20 from './brown20.png';
-import brown21 from './brown21.png';
+const brown1 = 'brown01.png';
+const brown2 = 'brown02.png';
+const brown3 = 'brown03.png';
+const brown4 = 'brown04.png';
+const brown5 = 'brown05.png';
+const brown6 = 'brown06.png';
+const brown7 = 'brown07.png';
+const brown8 = 'brown08.png';
+const brown9 = 'brown09.png';
+const brown10 = 'brown10.png';
+const brown11 = 'brown11.png';
+const brown12 = 'brown12.png';
+const brown13 = 'brown13.png';
+const brown14 = 'brown14.png';
+const brown15 = 'brown15.png';
+const brown16 = 'brown16.png';
+const brown17 = 'brown17.png';
+const brown18 = 'brown18.png';
+const brown19 = 'brown19.png';
+const brown20 = 'brown20.png';
+const brown21 = 'brown21.png';
 
-const cards = {
-  brown1,
-  brown2,
-  brown3,
-  brown4,
-  brown5,
-  brown6,
-  brown7,
-  brown8,
-  brown9,
-  brown10,
-  brown11,
-  brown12,
-  brown13,
-  brown14,
-  brown15,
-  brown16,
-  brown17,
-  brown18,
-  brown19,
-  brown20,
-  brown21,
+const cardsBrown = {
+  1: brown1,
+  2: brown2,
+  3: brown3,
+  4: brown4,
+  5: brown5,
+  6: brown6,
+  7: brown7,
+  8: brown8,
+  9: brown9,
+  10: brown10,
+  11: brown11,
+  12: brown12,
+  13: brown13,
+  14: brown14,
+  15: brown15,
+  16: brown16,
+  17: brown17,
+  18: brown18,
+  19: brown19,
+  20: brown20,
+  21: brown21,
 }
 
-export default cards
+export default cardsBrown
--- assets/MythicCards/green/index.js
+++ assets/MythicCards/green/index.js
@@ -1,41 +1,41 @@
-import green1 from './green1.png';
-import green2 from './green2.png';
-import green3 from './green3.png';
-import green4 from './green4.png';
-import green5 from './green5.png';
-import green6 from './green6.png';
-import green7 from './green7.png';
-import green8 from './green8.png';
-import green9 from './green9.png';
-import green10 from './green10.png';
-import green11 from './green11.png';
-import green12 from './green12.png';
-import green13 from './green13.png';
-import green14 from './green14.png';
-import green15 from './green15.png';
-import green16 from './green16.png';
-import green17 from './green17.png';
-import green18 from './green18.png';
+const green1 = 'green01.png';
+const green2 = 'green02.png';
+const green3 = 'green03.png';
+const green4 = 'green04.png';
+const green5 = 'green05.png';
+const green6 = 'green06.png';
+const green7 = 'green07.png';
+const green8 = 'green08.png';
+const green9 = 'green09.png';
+const green10 = 'green10.png';
+const green11 = 'green11.png';
+const green12 = 'green12.png';
+const green13 = 'green13.png';
+const green14 = 'green14.png';
+const green15 = 'green15.png';
+const green16 = 'green16.png';
+const green17 = 'green17.png';
+const green18 = 'green18.png';
 
-const cards = {
-  green1,
-  green2,
-  green3,
-  green4,
-  green5,
-  green6,
-  green7,
-  green8,
-  green9,
-  green10,
-  green11,
-  green12,
-  green13,
-  green14,
-  green15,
-  green16,
-  green17,
-  green18,
+const cardsGreen = {
+  1: green1,
+  2: green2,
+  3: green3,
+  4: green4,
+  5: green5,
+  6: green6,
+  7: green7,
+  8: green8,
+  9: green9,
+  10: green10,
+  11: green11,
+  12: green12,
+  13: green13,
+  14: green14,
+  15: green15,
+  16: green16,
+  17: green17,
+  18: green18,
 };
 
-export default cards;
+export default cardsGreen
